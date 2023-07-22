5a14020efc2a21ac56076164dafe21994cf47e38
Aman Tulsyan <amantulsyan35@gmail.com>
2023-07-23 03:15:23 +0530

    update

--- .wundergraph/operations/users/get.ts
+++ .wundergraph/operations/users/get.ts
@@ -1,14 +1,14 @@
-import { createOperation, z } from '../../generated/wundergraph.factory';
+import { createOperation, z } from "../../generated/wundergraph.factory";
 
 export default createOperation.query({
-	input: z.object({
-		id: z.string(),
-	}),
-	handler: async ({ input }) => {
-		return {
-			id: input.id,
-			name: 'Jens',
-			bio: 'Founder of WunderGraph',
-		};
-	},
+  input: z.object({
+    id: z.string(),
+  }),
+  handler: async ({ input }) => {
+    return {
+      id: input.id,
+      name: "Jens",
+      bio: "Founder of WunderGraph",
+    };
+  },
 });
